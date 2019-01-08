74115089e382197abf508da4a46035ceb6d6893f
blue271828 <22131684+blue271828@users.noreply.github.com>
2019-01-08 20:00:07 +0900

    Enable bash completion

--- config/bash/10_bash.sh
+++ config/bash/10_bash.sh
@@ -3,3 +3,5 @@
 export PS1='\[\e[32m\]\u@\H \e[33m\w\[\e[0m\]\n\\$ \[$(tput sgr0)\]'
 
 export INPUTRC="$XDG_CONFIG_HOME"/readline/inputrc
+
+[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
