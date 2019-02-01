7f5debee3308ed63b4e4706ed1a096952290a257
blue271828 <22131684+blue271828@users.noreply.github.com>
2019-02-01 11:25:28 +0900

    Visible exit code on prompt

--- config/bash/10_bash.sh
+++ config/bash/10_bash.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-export PS1='\[\e[32m\]\u@\H \e[33m\w\[\e[0m\]\n\\$ \[$(tput sgr0)\]'
+
+PROMPT_COMMAND=__prompt_command
+
+__prompt_command() {
+  local EXIT="$?"
+
+  local RCol='\[\e[0m\]'
+  local Red='\[\e[31m\]'
+  local Gre='\[\e[32m\]'
+  local Yel='\[\e[33m\]'
+
+  if [ $EXIT == 0 ]; then
+    PS1="${Gre}${EXIT} ->"
+  else
+    PS1="${Red}${EXIT} ->"
+  fi
+
+  PS1+=" ${Gre}\u@\H:${Yel}\w${RCol}\n\\$ \[$(tput sgr0)\]"
+}
+
 
 export INPUTRC="$XDG_CONFIG_HOME"/readline/inputrc
+
 
 [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
