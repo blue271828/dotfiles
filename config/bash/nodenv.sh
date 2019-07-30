c2bb940f578ad9bdb06d41bbc22b16fc583baf62
blue271828 <22131684+blue271828@users.noreply.github.com>
2019-07-30 22:02:18 +0900

    Add configs of nodenv for bash and npm

---
+++ config/bash/nodenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if type nodenv >/dev/null 2>&1
+then
+  export NODENV_ROOT="$XDG_DATA_HOME"/nodenv
+  eval "$(nodenv init -)"
+
+  export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
+fi
