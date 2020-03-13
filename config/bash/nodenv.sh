5c3d2e8318c10a2d177c58f8765ddabf63914b4e
blue271828 <22131684+blue271828@users.noreply.github.com>
2020-03-13 13:25:26 +0900

    Set a npm bin PATH

--- config/bash/nodenv.sh
+++ config/bash/nodenv.sh
@@ -4,7 +4,10 @@ if type nodenv >/dev/null 2>&1
 then
   export NODENV_ROOT="$XDG_DATA_HOME"/nodenv
   eval "$(nodenv init -)"
+fi
 
+if type node >/dev/null 2>&1
+then
   export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
-  export PATH=$PATH:node_modules/.bin
+  export PATH="$PATH:$(npm bin --global 2>/dev/null)"
 fi
